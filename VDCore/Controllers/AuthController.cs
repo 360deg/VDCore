3883cecbed9427470e64d82900289ec455f88a66
Vlad Pecherytsia <osa.9735@gmail.com>
2021-04-27 12:44:08 +0300

    Added jwt token generation. Added swagger token support.

--- VDCore/Controllers/AuthController.cs
+++ VDCore/Controllers/AuthController.cs
@@ -1,9 +1,90 @@
 using Microsoft.AspNetCore.Mvc;
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text.Json;
+using Microsoft.AspNetCore.Authorization;
+using VDCore.Authorization;
+
 namespace VDCore.Controllers
 {
+    [Route("[controller]")]
     public class AuthController: ControllerBase
     {
+        // тестовые данные вместо использования базы данных
+        private List<Person> people = new List<Person>
+        {
+            new Person { Login="admin@gmail.com", Password="12345", Role = "admin" },
+            new Person { Login="qwerty@gmail.com", Password="55555", Role = "user" }
+        };
         
+        [HttpPost("/token")]
+        public IActionResult Token(string username, string password)
+        {
+            var identity = GetIdentity(username, password);
+            if (identity == null)
+            {
+                return BadRequest(new { errorText = "Invalid username or password." });
+            }
+ 
+            var now = DateTime.UtcNow;
+            var jwt = new JwtSecurityToken(
+                issuer: AuthOptions.ISSUER,
+                audience: AuthOptions.AUDIENCE,
+                notBefore: now,
+                claims: identity.Claims,
+                expires: now.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
+                signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+ 
+            var response = new
+            {
+                access_token = encodedJwt,
+                username = identity.Name
+            };
+            
+            return Accepted(response);
+        }
+        
+        private ClaimsIdentity GetIdentity(string username, string password)
+        {
+            Person person = people.FirstOrDefault(x => x.Login == username && x.Password == password);
+            if (person != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimsIdentity.DefaultNameClaimType, person.Login),
+                    new Claim(ClaimsIdentity.DefaultRoleClaimType, person.Role)
+                };
+                ClaimsIdentity claimsIdentity =
+                    new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
+                        ClaimsIdentity.DefaultRoleClaimType);
+                return claimsIdentity;
+            }
+ 
+            // If user not found
+            return null;
+        }
+        
+        [Authorize]
+        [HttpGet]
+        [Route("getlogin")]
+        public IActionResult GetLogin()
+        {
+            return Ok($"Ваш логин: {User.Identity.Name}");
+        }
+         
+        [Authorize(Roles = "admin")]
+        [HttpGet]
+        [Route("getrole")]
+        public IActionResult GetRole()
+        {
+            return Ok("Ваша роль: администратор");
+        }
     }
 }
